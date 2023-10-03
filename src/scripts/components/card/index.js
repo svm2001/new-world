75a3072f5ec916f9b44eae81454db0c313ad1c28
Slava <iblancos.instagram@gmail.com>
2023-10-03 12:08:04 +0400

    ..

--- src/scripts/components/card/index.js
+++ src/scripts/components/card/index.js
@@ -3,6 +3,32 @@ export function card() {
   if (addToFavouriteBtn) {
     addToFavouriteBtn.forEach(item => {
       item.addEventListener('click', () => {
+        var favorID = $(this).attr('data-item')
+        addFavorite(favorID);
+        function addFavorite(id, action)
+        {
+          var param = 'id='+id;
+
+          $.ajax({
+            url:     '/local/templates/novmir_2023/assets/scripts/favorites.php', // URL отправки запроса
+            type:     "GET",
+            dataType: "json",
+            data: param,
+            success: function(response) { // Если Данные отправлены успешно
+
+              if(response.ELEMENTS.length){
+                $(`.fav_count`).text(response.ELEMENTS.length)
+                $(`.fav_counter`).addClass("visible");
+              }else{
+                $(`.fav_count`).text(Object.keys(response.ELEMENTS).length)
+                $(`.favor-counter`).removeClass("visible");
+              }
+            },
+            error: function(jqXHR, textStatus, errorThrown){ // Ошибка
+              console.log('Error: '+ errorThrown);
+            }
+          });
+        }
         item.classList.toggle('active')
       })
     })
